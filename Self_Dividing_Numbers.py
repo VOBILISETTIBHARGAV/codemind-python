c2316a665a2cde826eafe6fd4c0ba1ec256faf12
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-11 17:19:30 +0530

    a=int(input()) b=int(input()) for i in range(a,b+1):     v=i     j=i     s=0     count=0     while(i>0):       s=i%10       count=count+1       i=i//10     c=0     while j>0:         r=j%10         if (r!=0 and v%r==0) :             c=c+1         j=j//10     if(count==c):         print(v,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,21 +1,19 @@
-def count(n):
-    su=0
-    while n>0:
-        n=n//10
-        su+=1
-    return su
-def dividing(n):
-    k=n
-    su=0
-    while n>0:
-        temp=n%10
-        if temp>0:
-            if k%temp==0:
-                su+=1
-        n=n//10
-    return su
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    if dividing(i)==count(i):
-        print(i,end=' ')
+    v=i
+    j=i
+    s=0
+    count=0
+    while(i>0):
+      s=i%10
+      count=count+1
+      i=i//10
+    c=0
+    while j>0:
+        r=j%10
+        if (r!=0 and v%r==0) :
+            c=c+1
+        j=j//10
+    if(count==c):
+        print(v,end=" ")
