8d743ff9735918c24b416ce6f44f5949fd146e31
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-12 23:36:50 +0530

    a=int(input()) l=list(map(int,input().split())) k=int(input()) c=0 su=0 x=[] l1=set(l) l2=list(l1) for i in range(len(l2)):     c=0     for j in range(len(l)):         if l2[i]==l[j]:             c+=1     if c==k:         x.append(l2[i])         su+=1 if su==0:     print('-1') else:     print(*x)

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -1,17 +1,17 @@
 a=int(input())
 l=list(map(int,input().split()))
 k=int(input())
-count=0
+c=0
 su=0
 x=[]
 l1=set(l)
 l2=list(l1)
 for i in range(len(l2)):
-    count=0
+    c=0
     for j in range(len(l)):
         if l2[i]==l[j]:
-            count+=1
-    if count==k:
+            c+=1
+    if c==k:
         x.append(l2[i])
         su+=1
 if su==0:
