0633f9254a87048aca59861e2718fcfec2f93e6a
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-14 20:53:36 +0530

    x=input() for i in x:     if x.count(i)==1:         print(i)         break else:     print(-1)

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,17 +1,7 @@
-a=input()
-a=a.lower()
-count=0
-su=0
-x=[]
-for i in range(len(a)):
-    count=0
-    for j in range(len(a)):
-        if a[i]==a[j]:
-            count+=1
-    if count==1:
-        x.append(a[i])
-        su+=1
-if su==0:
-    print("-1")
+x=input()
+for i in x:
+    if x.count(i)==1:
+        print(i)
+        break
 else:
-    print(x[0])
+    print(-1)
