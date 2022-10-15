894e88d28e41fb139fa2dbdc948303fb712f294a
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-16 00:17:35 +0530

    a=input() x=[] c=0 for i in range(len(a)):     if a[i]=='a' or a[i]=='e' or a[i]=='i' or a[i]=='o' or a[i]=='u':         x.append(a[i]) if 'a' in x:     c+=1 if 'e' in x:     c+=1 if 'i' in x:     c+=1 if 'o' in x:     c+=1 if 'u' in x:     c+=1 if c==5:     print(True) else:     print(False)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,20 +1,20 @@
 a=input()
 x=[]
-count=0
+c=0
 for i in range(len(a)):
-    if a[i]=='a' or a[i]=='i' or a[i]=='e' or a[i]=='o' or a[i]=='u':
+    if a[i]=='a' or a[i]=='e' or a[i]=='i' or a[i]=='o' or a[i]=='u':
         x.append(a[i])
 if 'a' in x:
-    count+=1
-if 'i' in x:
-    count+=1
+    c+=1
 if 'e' in x:
-    count+=1
+    c+=1
+if 'i' in x:
+    c+=1
 if 'o' in x:
-    count+=1
+    c+=1
 if 'u' in x:
-    count+=1
-if count==5:
+    c+=1
+if c==5:
     print(True)
 else:
     print(False)
