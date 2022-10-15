70cd6ed3ff26005dbd288b1e8ef06af80eb0759a
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-15 23:41:54 +0530

    x=int(input()) l=list(map(int,input().split())) x=[] c=0 for i in range(len(l)):     c=0     for j in range(len(l)):         if l[i]==l[j]:             c+=1     if l[i] not in x:             print(l[i],c,end=" ")             x.append(l[i])

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,12 +1,12 @@
-a=int(input())
+x=int(input())
 l=list(map(int,input().split()))
 x=[]
 c=0
-for i in range(a):
+for i in range(len(l)):
     c=0
-    for j in range(a):
+    for j in range(len(l)):
         if l[i]==l[j]:
             c+=1
     if l[i] not in x:
-        print(l[i],c,end=' ')
-        x.append(l[i])
+            print(l[i],c,end=" ")
+            x.append(l[i])
