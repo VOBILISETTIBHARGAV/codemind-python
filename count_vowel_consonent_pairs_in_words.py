1d749847288734632c5fe55e7b618c2962f6c11f
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-15 14:37:16 +0530

    a=input() a=a.lower() a=a.split() c=0 for i in range(len(a)):     x=str(a[i])     for i in range(len(x)):         if (x[i] in 'aeiou' and x[len(x)-i-1] not in 'aeiou') or (x[i] not in 'aeiou' and x[len(x)-i-1]  in 'aeiou'):             c+=1 print(c//2)

--- count_vowel_consonent_pairs_in_words.py
+++ count_vowel_consonent_pairs_in_words.py
@@ -1,10 +1,10 @@
 a=input()
 a=a.lower()
 a=a.split()
-count=0
+c=0
 for i in range(len(a)):
     x=str(a[i])
     for i in range(len(x)):
         if (x[i] in 'aeiou' and x[len(x)-i-1] not in 'aeiou') or (x[i] not in 'aeiou' and x[len(x)-i-1]  in 'aeiou'):
-            count+=1
-print(count//2)
+            c+=1
+print(c//2)
