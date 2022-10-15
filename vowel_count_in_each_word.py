94680ce9c5bd74a80e976b866380a35a7a87b362
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-15 14:45:16 +0530

    x=input() x=x.split() c=0 for i in range(len(x)):     c=0     a=str(x[i])     for i in range(len(a)):         if a[i] in 'aeiou':             c+=1     print(c,end=' ')

--- vowel_count_in_each_word.py
+++ vowel_count_in_each_word.py
@@ -1,10 +1,10 @@
-a=input()
-a=a.split()
-count=0
-for i in range(len(a)):
-    count=0
-    x=str(a[i])
-    for i in range(len(x)):
-        if x[i] in 'aieou':
-            count+=1
-    print(count,end=' ')
+x=input()
+x=x.split()
+c=0
+for i in range(len(x)):
+    c=0
+    a=str(x[i])
+    for i in range(len(a)):
+        if a[i] in 'aeiou':
+            c+=1
+    print(c,end=' ')
