a3e61cb91c1b83e28d8ae94ab277b1c545b8160c
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-15 13:33:28 +0530

    a=int(input()) l=list(map(int,input().split())) x=[] for i in range(len(l)):     for j in range(len(l)):         if l[i]==l[j]:             if l[i] not in x:                 x.append(l[i]) print(*x)

--- unique_elements.py
+++ unique_elements.py
@@ -1,9 +1,9 @@
-x=int(input())
+a=int(input())
 l=list(map(int,input().split()))
-a=[]
+x=[]
 for i in range(len(l)):
     for j in range(len(l)):
         if l[i]==l[j]:
-            if l[i] not in a:
-                a.append(l[i])
-print(*a)
+            if l[i] not in x:
+                x.append(l[i])
+print(*x)
