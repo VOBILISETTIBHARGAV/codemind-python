e5d192475d14ba0fed6a77efaf0ba26eb73c9043
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-15 13:57:09 +0530

    a=int(input()) l=list(map(int,input().split())) x=[] y=[] c=0 for i in range(len(l)):     if l[i]%2==0:         x.append(l[i])     else:         y.append(l[i]) if len(x)>len(y):     for i in range(len(y)):         print(x[i],y[i],end=" ")         c+=1     for i in range(c,len(x)):         print(x[i],end=" ")     if len(l)%2!=0:         print("0") if len(y)>len(x):     for i in range(len(x)):         print(x[i],y[i],end=" ")         c+=1     for i in range(c,len(y)):         print(y[i],end=" ")     if len(l)%2!=0:         print("0") elif len(x)==len(y):     for i in range(len(x)):         print(x[i],y[i],end=" ")

---
+++ even_odd_pairs.py
@@ -0,0 +1,29 @@
+a=int(input())
+l=list(map(int,input().split()))
+x=[]
+y=[]
+c=0
+for i in range(len(l)):
+    if l[i]%2==0:
+        x.append(l[i])
+    else:
+        y.append(l[i])
+if len(x)>len(y):
+    for i in range(len(y)):
+        print(x[i],y[i],end=" ")
+        c+=1
+    for i in range(c,len(x)):
+        print(x[i],end=" ")
+    if len(l)%2!=0:
+        print("0")
+if len(y)>len(x):
+    for i in range(len(x)):
+        print(x[i],y[i],end=" ")
+        c+=1
+    for i in range(c,len(y)):
+        print(y[i],end=" ")
+    if len(l)%2!=0:
+        print("0")
+elif len(x)==len(y):
+    for i in range(len(x)):
+        print(x[i],y[i],end=" ")
