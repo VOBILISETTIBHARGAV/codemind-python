487f07d59b60aaa8c3ee3a5171e7e2e7176052d1
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-15 13:33:03 +0530

    a=int(input()) l=list(map(int,input().split())) x=[] count=0 for i in range(len(l)):     count=0     for j in range(len(l)):         if l[i]==l[j]:             count+=1     if count==l[i]:         if l[i] not in x:             x.append(l[i]) if len(x)==0:     print("-1") else:     print(*x)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,13 +1,13 @@
 a=int(input())
 l=list(map(int,input().split()))
 x=[]
-c=0
+count=0
 for i in range(len(l)):
-    c=0
+    count=0
     for j in range(len(l)):
         if l[i]==l[j]:
-            c+=1
-    if c==l[i]:
+            count+=1
+    if count==l[i]:
         if l[i] not in x:
             x.append(l[i])
 if len(x)==0:
