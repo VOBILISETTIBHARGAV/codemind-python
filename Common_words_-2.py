1ca856db6e71b379616a51a136a0dfaea892eb23
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-15 14:26:49 +0530

    a=input() b=input() a=a.lower() b=b.lower() a=a.split() b=b.split() x=[] y=[] c=0 su=0 for i in range(len(a)):     c=0     for j in range(len(a)):         if a[i]==a[j]:             c+=1     if c==1:         x.append(a[i]) for i in range(len(b)):     c=0     for j in range(len(b)):         if b[i]==b[j]:             c+=1     if c==1:         y.append(b[i]) for i in range(len(x)):     if x[i] in y:         su+=1 print(su)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -6,21 +6,21 @@ a=a.split()
 b=b.split()
 x=[]
 y=[]
-count=0
+c=0
 su=0
 for i in range(len(a)):
-    count=0
+    c=0
     for j in range(len(a)):
         if a[i]==a[j]:
-            count+=1
-    if count==1:
+            c+=1
+    if c==1:
         x.append(a[i])
 for i in range(len(b)):
-    count=0
+    c=0
     for j in range(len(b)):
         if b[i]==b[j]:
-            count+=1
-    if count==1:
+            c+=1
+    if c==1:
         y.append(b[i])
 for i in range(len(x)):
     if x[i] in y:
