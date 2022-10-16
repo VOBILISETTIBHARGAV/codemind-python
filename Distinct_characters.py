5e5d9a97d8a3a492debde5d473b291e46e9089a2
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-10-16 10:52:36 +0530

    z=input() z=z.lower() c=0 q="" for i in z:     if i not in q and z.count(i)==1 and i!=' ':         q+=i x="".join(sorted(q)) print(x)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,7 +1,9 @@
-x=input()
-x=x.lower()
-a=set(x)
-l=sorted(a)
-for i in range(len(l)):
-    if ord(l[i])!=32:
-        print(l[i],end='')
+z=input()
+z=z.lower()
+c=0
+q=""
+for i in z:
+    if i not in q and z.count(i)==1 and i!=' ':
+        q+=i
+x="".join(sorted(q))
+print(x)
